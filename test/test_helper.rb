4d3238680f4f9b1f99bafd54c4fcd78e3de6fdde
Susan Prestage <susan.prestage@gmail.com>
2013-10-09 13:31:47 -0700

    Section 5 and 6 of Getting Started with Rails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/rails"
+require "minitest/rails/capybara"
+
+# Uncomment for awesome colorful output
+require "minitest/pride"
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
@@ -12,4 +17,11 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  include Rails.application.routes.url_helpers
+  # include Capybara::RSpecMatchers
+  include Capybara::DSL
+  include Capybara::Assertions
 end
